ffe272fd775b2eeb722be725f7673507273b524a
Suraj Kumar <suraj6472@gmail.com>
2022-07-23 23:17:03 +0530

    usereducer another example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React, { useReducer } from "react";
 import ReactDOM from "react-dom";
 
+const initialState = {
+  message: "hi",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "YELL":
+      return {
+        message: `hey! i just said ${state.message}`,
+      };
+
+    case "whisper":
+      return {
+        message: `excuse me, I just said ${state.message}`,
+      };
+  }
+}
+
 function App() {
-    const [checked, toggle] = useReducer((checked) => !checked, false);
-    return (
-      <>
-        <input type="checkbox" onChange={toggle} value={checked} />{" "}
-        {checked ? "checked" : "unchecked"}
-      </>
-    );
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <p>{state.message}</p>
+      <button onClick={() => dispatch({ type: "YELL" })}>YEll</button>
+      <button onClick={() => dispatch({ type: "whisper" })}>whisper</button>
+    </>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
