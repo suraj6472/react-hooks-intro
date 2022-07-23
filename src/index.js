5f2c284f0062af75856c3c81130ff1fd258df4c2
Suraj Kumar <suraj6472@gmail.com>
2022-07-23 18:46:35 +0530

    displaying stars

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+// import App from "./App";
+import { FaStar } from "react-icons/fa";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const createArray = (length) => [...Array(length)];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />;
+}
+
+function StarRating({ totalStars }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} out of {totalStars} stars
+      </p>
+    </>
+  );
+}
+
+function App() {
+  return <StarRating totalStars={10} />;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
