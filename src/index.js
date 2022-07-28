366ebf023c50cf7be7124c38647c97bc6f8bfb78
Suraj Kumar <suraj6472@gmail.com>
2022-07-28 22:16:43 +0530

    custom input hooks - example

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import { useInput } from "./useInput";
 
 function App() {
-  const [sound, setSound] = useState('');
-  const [color, setColor] = useState('#000000');
+  const [soundProps, resetSound] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
   function submitHandler(e) {
-    e.preventDefault(); 
-    alert(`${sound}  -- ${color}`);
-    setColor('#000000')
-    setSound('');
+    e.preventDefault();
+    alert(`${soundProps.value}  -- ${colorProps.value}`);
+    resetColor();
+    resetSound();
   }
 
   return (
     <form onSubmit={submitHandler}>
-      <input type="text" onChange={(e) => setSound(e.target.value)} value={sound} />
-      <input type="color" onChange={(e) => setColor(e.target.value)} value={color} />
+      <input type="text" {...soundProps} />
+      <input type="color" {...colorProps} />
       <button>Add</button>
     </form>
   );
